1bf799d3d5add28cb1903e70315669c67433c424
agostinhosamil <agostinhosaml832@gmail.com>
2023-11-27 16:34:25 +0100

    feat: update workflow package publish config

---
+++ packages/production/vendor/dist-package-files.js
@@ -0,0 +1,17 @@
+const path = require('path')
+
+const { copyFileTo } = require('@asaml/fs-helpers')
+
+const files = [
+  'README.md',
+  'package.json'
+]
+
+for (const fileName of files) {
+  const filePath = path.resolve(__dirname, '..', fileName)
+
+  copyFileTo({
+    filePath,
+    destDir: '../dist'
+  })
+}
