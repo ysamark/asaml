43569987aee17ac040d0a1446e2f5f526029f68d
agostinhosamil <agostinhosaml832@gmail.com>
2023-12-03 13:03:24 +0100

    fix(args): use an empty object notation as de default options

--- packages/development/src/get-package-dist.js
+++ packages/development/src/get-package-dist.js
@@ -1,6 +1,12 @@
 import path from 'path'
 
-export function getPackageDist ({ packageDir }) {
+export function getPackageDist (options) {
+  if (!(typeof options === 'object')) {
+    options = {}
+  }
+
+  const { packageDir } = options
+
   try {
     const rootDir = typeof 'str' === typeof packageDir ? packageDir : process.cwd()
     const packageJsonFilePath = path.resolve(rootDir, 'package.json')
