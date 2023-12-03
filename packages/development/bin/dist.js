cb81b67a9634a82ed54031bb69e22874c2af6596
agostinhosamil <agostinhosaml832@gmail.com>
2023-12-03 12:35:43 +0100

    fix: use package dist list getter helper

--- packages/development/bin/dist.js
+++ packages/development/bin/dist.js
@@ -1,16 +1,6 @@
 #!/usr/bin/env node
 
-const path = require('path')
+const { distPackageFiles } = require('../dist-package-files')
+const { getPackageDist } = require('../get-package-dist')
 
-const { distPackageFiles } = require('../dist/dist-package-files')
-
-try {
-  const packageJsonFile = require(path.resolve(process.cwd(), 'package.json'))
-
-  if (packageJsonFile.dist instanceof Array) {
-    distPackageFiles(packageJsonFile.dist)
-  }
-} catch (err) {
-} finally {
-  console.log('\nDone!!\n')
-}
+distPackageFiles(getPackageDist())
